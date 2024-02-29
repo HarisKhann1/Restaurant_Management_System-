292f59222612988e74482d80bbc40cb22ee49d56
Haris Khan <hariskhan3428@gamil.com>
2024-02-29 19:58:23 +0500

    sort function and update add dish function

--- RMS.py
+++ RMS.py
@@ -1,6 +1,5 @@
 import os
 
-
 def heading(name):
     os.system('cls')
     print(f'\n--------------------- {name} -----------------------\n')
@@ -29,7 +28,10 @@ def add_dish():
         except Exception as e:
             print("Error: Can't store data in file ...", e)
 
+        closeLine()
+
 def load_data_from_file():
+
     database = ''
     with open('dbRMS.txt', 'r') as file:
         try:
@@ -41,15 +43,16 @@ def load_data_from_file():
 
 # displaying all dishes with price from database (dbRMS)
 def display_menu():
+
     heading('Menu')
     database = load_data_from_file()
 
     for count,content in enumerate(database, 1):
-        print(f' {count}) {content}')
+        print(f' {count}. {content}')
 
     closeLine()
 
-def print_bill(customer_dish_num, content):
+def print_bill(customer_dish_num, content, dish_countity):
     customer_order = []
     prices = []
     for i in customer_dish_num:
@@ -57,8 +60,8 @@ def print_bill(customer_dish_num, content):
         
     os.system('cls')
     heading('Bill')
-    for i in customer_order:
-        print(i)
+    for num, i in enumerate(customer_order):
+        print(f'{num+1}. {i}countity : {dish_countity[num]}\n')
 
     for item in customer_order:
         parts = item.split(':')          
@@ -66,8 +69,8 @@ def print_bill(customer_dish_num, content):
         prices.append(int(price))
 
     total = 0
-    for i in prices:
-        total = total + i
+    for num, value in enumerate(prices):
+        total = total + (value * dish_countity[num])
         
     closeLine()
     print(f'Total bill: {total}')
@@ -76,6 +79,7 @@ def print_bill(customer_dish_num, content):
 def generate_bill():
 
     customer_dish_num = []  
+    dish_countity = []
     content = load_data_from_file()
     
     heading('Generate Bill')
@@ -84,13 +88,17 @@ def generate_bill():
     while True:
         # taking input for dish and its price
         dish_num = int(input("Enter dish number: "))
+        dish_cout = int(input("Enter dish countity: "))
         yes_no = input("Another dish to bill (yes/no): ")
+
         customer_dish_num.append(dish_num-1)
+        dish_countity.append(dish_cout)
 
         if yes_no == 'no':
             print("Bill generated successfully ...")
             break
-    print_bill(customer_dish_num, content)
+
+    print_bill(customer_dish_num, content, dish_countity)
 
 def search_dish():
 
@@ -136,7 +144,6 @@ def delete_dish():
         with open('dbRMS.txt', 'w') as file:
             for item in new_dish_list:
                 file.writelines(item)
-        # print(new_dish_list)
 
         print(f'\nDish Delete Successfully ...')
         closeLine()
@@ -144,7 +151,19 @@ def delete_dish():
     except ValueError:
             print("\nError: Dish not found ...", )
             closeLine()
+
+def sort_key_fun(item):
+    return int(item.split(':')[1])
+
+def sort_by_alpha():
+    heading('Sort')
+    content = load_data_from_file()
+    sorted_list = sorted(content, key=sort_key_fun)
     
+    for num, item in enumerate(sorted_list,1):
+        print(f'{num}. {item}')
+    closeLine()
+
 def main():
     lst = ['Add Dish', 'show Dish', 'Generate Bill', 'Search Dish', 'Delete Dish', 'Sort dish', 'Exit']
     # displaying above list
@@ -171,6 +190,8 @@ def main():
                     search_dish()
                 case 5:
                     delete_dish()
+                case 6:
+                    sort_by_alpha()
 
         wait = input('\nPress Enter ...')
     
